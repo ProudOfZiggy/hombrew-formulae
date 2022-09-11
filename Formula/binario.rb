0119df40251566a18e34f7d12aa06dcf6503be5b
Niktia Rodionov <>
2022-09-12 00:30:41 +0900

    Update version

--- Formula/binario.rb
+++ Formula/binario.rb
@@ -1,15 +1,16 @@
  class Binario < Formula
   desc "A tool for automatizarion of generation binary swift packages from external SPM dependencies"
   homepage "https://github.com/ProudOfZiggy/Binario"
-  version "0.80.2"
+  version "0.80.3"
 
   url "https://github.com/ProudOfZiggy/Binario.git",
-      tag: "#{version}", revision: "9c67a368aa998fd54125ff50394b91296397cda9"
+      tag: "#{version}", revision: "ed6c4530a00757b3848208e4af7a8941a83e46c0"
   head "https://github.com/ProudOfZiggy/Binario.git"
 
   depends_on xcode: ["12.0", :build]
 
   def install
-    system "make", "install"
+    system "make", "build"
+    bin.install ".build/release/binario"
   end
 end
