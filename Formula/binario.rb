14d66b17686e95df105d271d6387caf7415e6e91
Niktia Rodionov <>
2022-09-12 02:06:18 +0900

    Update install method and version

--- Formula/binario.rb
+++ Formula/binario.rb
@@ -1,16 +1,11 @@
  class Binario < Formula
   desc "A tool for automatizarion of generation binary swift packages from external SPM dependencies"
   homepage "https://github.com/ProudOfZiggy/Binario"
-  version "0.80.3"
+  version "0.80.9"
 
-  url "https://github.com/ProudOfZiggy/Binario.git",
-      tag: "#{version}", revision: "ed6c4530a00757b3848208e4af7a8941a83e46c0"
-  head "https://github.com/ProudOfZiggy/Binario.git"
-
-  depends_on xcode: ["12.0", :build]
+  url "https://github.com/ProudOfZiggy/Binario/releases/download/v#{version}/binario.tar.gz"
 
   def install
-    system "make", "build"
-    bin.install ".build/release/binario"
+    bin.install "binario"
   end
 end
