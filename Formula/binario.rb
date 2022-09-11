6c75ce44eee07c344a910c46833bad4289d756f8
Niktia Rodionov <>
2022-09-11 20:17:41 +0900

    Formula

--- Formula/binario.rb
+++ Formula/binario.rb
@@ -1,32 +1,15 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Binario < Formula
+ class Binario < Formula
   desc "A tool for automatizarion of generation binary swift packages from external SPM dependencies"
   homepage "https://github.com/ProudOfZiggy/Binario"
-  url "https://github.com/ProudOfZiggy/Binario.git"
   version "0.80.1"
 
-  depends_on "xcode" => :build
+  url "https://github.com/ProudOfZiggy/Binario.git",
+      tag: "#{version}", revision: "6fe2d9b1f6e8fb7d500008e510a677fe299f7e9b"
+  head "https://github.com/ProudOfZiggy/Binario.git"
+
+  depends_on "xcode": :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test Binario`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "make", "install"
   end
 end
